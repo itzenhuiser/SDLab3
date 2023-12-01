c79d4c75ae4ba379eba05d59f54a9ac32e5565b6
Kyle McGuinness <k.mcguinness@intellisee.com>
2023-11-30 18:19:59 -0600

    server has session

--- server.js
+++ server.js
@@ -1,29 +1,47 @@
 const express = require('express');
+const session = require('express-session');
 const bodyParser = require('body-parser');
 const { getPassword } = require('./scripts');
 
 const app = express();
 
-app.use(express.static('.'));
+// Session middleware setup
+app.use(session({
+  secret: 'your_secret_key',
+  resave: false,
+  saveUninitialized: true
+}));
 
-// Middleware to parse the form data
+app.use(express.static('.'));
 app.use(bodyParser.urlencoded({ extended: false }));
 
+// Login route
 app.post('/login', async (req, res) => {
-    const { username, password } = req.body;
-    try {
-      const correctPassword = await getPassword(username);
-      if (correctPassword && password === correctPassword) {
-        console.log('Login successful');
-        res.redirect('/login_redirect.html'); // Redirect to the login result page
-      } else {
-        console.log('Login failed');
-        res.redirect('/login_failed_redirect.html');
-      }
-    } catch (err) {
-      console.error(err);
-      res.status(500).send('An error occurred');
+  const { username, password } = req.body;
+  try {
+    const correctPassword = await getPassword(username);
+    if (correctPassword && password === correctPassword) {
+      // Save the username in the session
+      req.session.username = username;
+      console.log('Login successful');
+      res.redirect('/login_redirect.html'); // Redirect to the login result page
+    } else {
+      console.log('Login failed');
+      res.redirect('/login_failed_redirect.html');
     }
-  });
+  } catch (err) {
+    console.error(err);
+    res.status(500).send('An error occurred');
+  }
+});
+
+// Example route that requires user to be logged in
+app.get('/profile', (req, res) => {
+  if (req.session.username) {
+    res.send(`Welcome, ${req.session.username}!`);
+  } else {
+    res.redirect('/login.html'); // Redirect to the login page if not logged in
+  }
+});
 
 app.listen(8000, () => console.log('Server is running on port 8000'));
